7b293dad83211ff5c8f739ca6f330c29ac75e93c
Paleo <paleo@paleo.casa>
2020-01-21 12:12:29 +0100

    Remove bundles from git.

--- handledom/template-string.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, '__esModule', { value: true });
-
-function handledom(strings, ...expressions) {
-    throw new Error(`Configure the webpack loader or use "handledom/browser"`);
-}
-
-exports.default = handledom;
